6f9ab83b81be2ae75a64f21054cb6e562dc89599
Sharwon Pius <sharwonpiusm@gmail.com>
2018-08-11 00:19:24 +0530

    for debugging

---
+++ git_example.py
@@ -0,0 +1,18 @@
+########
+This python file si a goood starting pouint.
+Activation Funstions
+########
+
+#libraries
+import numpy as np
+
+def sigmoid(x):
+  # enter code below
+  return (1 / (1 + numpy.exp(-x)))
+
+def tanh(x):
+  # enter code below
+  
+def relu(x):
+  # entr code below
+  
